e7c4f7f59fefbb89c9d94ea2cacd72abe6a1e044
Elv0nne <elvonnetropes@gmail.com>
2026-07-17 23:47:45 +0700

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,6 +46,7 @@ dependencies {
     implementation("androidx.compose.material:material")
     implementation("androidx.compose.material:material-icons-extended")
     implementation("androidx.navigation:navigation-compose:2.8.0")
+    implementation("com.google.android.material:material:1.12.0")
 
     // Network
     implementation("com.squareup.retrofit2:retrofit:2.11.0")
